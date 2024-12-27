5b9ca4a2f45a6ecf7245dce608e2790f29e51ccb
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-12-27 23:12:14 +0600

    added keyboard_special_keys function: might have issue

--- game_logic.py
+++ game_logic.py
@@ -157,4 +157,88 @@ def mouseListener(button, state, x, y):
         elif 831 < x < 872 and 33 < y < 70:
             print("Goodbye")
             glutLeaveMainLoop()
-  
+
+
+# keyboard special key 
+def keyboard_special_keys(key, _, __):
+    global COUNT,LOOP,PAUSED,TREASURE_X1,TREASURE_Y1,TREASURE_X2,TREASURE_Y2,PLAYER_X,PLAYER_Y,RADIUS
+    if not PAUSED and LOOP==True and COUNT<MAX_MOVES:
+        if key == GLUT_KEY_LEFT:
+            if 250<=TREASURE_Y2<=350 and TREASURE_X2==500:
+                pass
+            elif TREASURE_Y2==400 and TREASURE_X2==600:
+                pass
+            elif TREASURE_X2==550 and 150<=TREASURE_Y2<=200:
+                pass
+            elif (TREASURE_X2==700 and TREASURE_Y2==350) or (TREASURE_X2==600 and TREASURE_Y2==300) or (TREASURE_X2==650 and TREASURE_Y2==200) or (TREASURE_X2==750 and TREASURE_Y2==250):
+                pass
+            elif  (TREASURE_X1-40<PLAYER_X and TREASURE_Y1<PLAYER_Y<TREASURE_Y2) and (((360<PLAYER_Y<400 or 260<PLAYER_Y<300) and PLAYER_X-40<550) or (210<PLAYER_Y<250 and PLAYER_X-40<450) or (160<PLAYER_Y<200 and PLAYER_X-40<600)):
+                pass 
+            else:
+                COUNT+=1
+                if TREASURE_X1>PLAYER_X and TREASURE_X1<PLAYER_X+40 and TREASURE_X2<PLAYER_X+80 and TREASURE_Y2>PLAYER_Y>TREASURE_Y1:
+                    PLAYER_X-=50
+                    draw_filled_circle(PLAYER_X,PLAYER_Y,RADIUS)
+                    
+                TREASURE_X1 -= 50
+                TREASURE_X2 -= 50
+                draw_filled_box(TREASURE_X1,TREASURE_Y1,TREASURE_X2,TREASURE_Y2)
+        elif key == GLUT_KEY_RIGHT: 
+            if TREASURE_X2==700 and 350<=TREASURE_Y2<=400:
+                pass
+            elif TREASURE_X2==750 and 200<=TREASURE_Y2<=300:
+                pass
+            elif TREASURE_X2==650 and TREASURE_Y2<=150:
+                pass
+            elif (TREASURE_X2==550 and TREASURE_Y2==200) or (TREASURE_X2==500 and TREASURE_Y2==300) or (TREASURE_X2==600 and TREASURE_Y2==350) or (TREASURE_X2==650 and TREASURE_Y2==250):
+                pass
+            elif  (TREASURE_X2+40>PLAYER_X and TREASURE_Y1<PLAYER_Y<TREASURE_Y2) and ((310<PLAYER_Y<350 and PLAYER_X+40>610) or (260<PLAYER_Y<300 and PLAYER_X+40>760) or (210<PLAYER_Y<250 and PLAYER_X+40>660) or (110<PLAYER_Y<150 and PLAYER_X+40>660)):
+                pass 
+            else:
+                COUNT+=1
+                if TREASURE_X2<PLAYER_X and TREASURE_X1>PLAYER_X-80 and TREASURE_X2>PLAYER_X-40 and TREASURE_Y2>PLAYER_Y>TREASURE_Y1:
+                    PLAYER_X+=50
+                    draw_filled_circle(PLAYER_X,PLAYER_Y,RADIUS)
+                TREASURE_X1 += 50
+                TREASURE_X2 += 50
+                draw_filled_box(TREASURE_X1,TREASURE_Y1,TREASURE_X2,TREASURE_Y2)
+        elif key == GLUT_KEY_DOWN:
+            if TREASURE_X2==500 and TREASURE_Y2==250:
+                pass
+            elif 550<=TREASURE_X2<=650 and TREASURE_Y2==150:
+                pass
+            elif 700<=TREASURE_X2<=750 and TREASURE_Y2==200:
+                pass
+            elif (TREASURE_X2==650 and TREASURE_Y2==400) or (TREASURE_X2==550 and TREASURE_Y2==350) or (TREASURE_X2==700 and TREASURE_Y2==300) or (TREASURE_X2==600 and TREASURE_Y2==250):
+                pass
+            elif  (TREASURE_Y1-40<PLAYER_Y and TREASURE_X1<PLAYER_X<TREASURE_X2) and ((460<PLAYER_X<500 and PLAYER_Y-40<200) or (620<PLAYER_X<650 and PLAYER_Y-40<100) or (550<PLAYER_X<600 and PLAYER_Y-40<210) or (650<PLAYER_X<700 and PLAYER_Y-40<250)):
+                pass    
+            else:
+                COUNT+=1
+                if TREASURE_Y1>PLAYER_Y and TREASURE_Y1<PLAYER_Y+40<TREASURE_Y2 and TREASURE_X2>PLAYER_X>TREASURE_X1:
+                    PLAYER_Y-=50
+                    draw_filled_circle(PLAYER_X,PLAYER_Y,RADIUS)
+                TREASURE_Y1 -= 50
+                TREASURE_Y2 -= 50
+                draw_filled_box(TREASURE_X1,TREASURE_Y1,TREASURE_X2,TREASURE_Y2)
+        elif key == GLUT_KEY_UP:
+            if (500<=TREASURE_X2<=550 and TREASURE_Y2==350): 
+                pass
+            elif  (TREASURE_Y2+40>PLAYER_Y and TREASURE_X1<PLAYER_X<TREASURE_X2) and ((560<PLAYER_X<600 and PLAYER_Y+40>400) or (710<PLAYER_X<750 and PLAYER_Y+40>300) or (610<PLAYER_X<650 and PLAYER_Y+40>300) or (510<PLAYER_X<550 and PLAYER_Y+40>250)):
+                pass    
+            elif (TREASURE_Y2==400 and 600<=TREASURE_X2<=700):
+                pass
+            elif (TREASURE_X2==750 and TREASURE_Y2==300):
+                pass  
+            elif (TREASURE_X2==600 and TREASURE_Y2==150) or (TREASURE_X2==700 and TREASURE_Y2==200) or (TREASURE_X2==650 and TREASURE_Y2==300) or (TREASURE_X2==550 and TREASURE_Y2==250):
+                pass
+            else:
+                COUNT+=1
+                if TREASURE_Y2<PLAYER_Y and TREASURE_Y1<PLAYER_Y-40<TREASURE_Y2 and TREASURE_X2>PLAYER_X>TREASURE_X1:
+                    PLAYER_Y+=50
+                    draw_filled_circle(PLAYER_X,PLAYER_Y,RADIUS)
+                TREASURE_Y1 += 50
+                TREASURE_Y2 += 50
+                draw_filled_box(TREASURE_X1,TREASURE_Y1,TREASURE_X2,TREASURE_Y2)
+
+        glutPostRedisplay()
